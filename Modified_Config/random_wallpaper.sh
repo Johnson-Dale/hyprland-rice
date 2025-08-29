e41c84b598c51296a9e9bfde4cdff5b8fd9aed9b
Johnson-Dale <djohnson298@students.avc.edu>
2025-08-28 17:29:27 -0700

    random wallpaper script, same as hyprpaper wiki example

---
+++ Modified_Config/random_wallpaper.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Sets wallpaper directory and checks currently loaded wallpaper
+WALLPAPER_DIR="$HOME/.wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
+
+# Gets a random wallpaper that isn`t the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALL")" | shuf -n 1)
+
+# Applies selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
